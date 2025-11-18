66a930583fad507b3bf4fce0cdac356416f14ac3
gampa-anupama <anupamagampa1@gmail.com>
2025-11-18 12:44:44 +0530

    Add greeting message 'hello' to App.java

--- src/main/java/SEGroup/MavenJavaProject/App.java
+++ src/main/java/SEGroup/MavenJavaProject/App.java
@@ -6,5 +6,6 @@ package SEGroup.MavenJavaProject;
 public class App {
     public static void main(String[] args) {
         System.out.println("Jai ganesh");
+        System.out.println("hello");
     }
 }
