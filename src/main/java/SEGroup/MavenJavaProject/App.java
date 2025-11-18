8dc9d653e4d20e46f2d44be41555a7f8034ff41c
gampa-anupama <anupamagampa1@gmail.com>
2025-11-18 12:46:26 +0530

    Update App.java

--- src/main/java/SEGroup/MavenJavaProject/App.java
+++ src/main/java/SEGroup/MavenJavaProject/App.java
@@ -7,5 +7,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Jai ganesh");
         System.out.println("hello");
+                System.out.println("hello");
+
     }
 }
